d46116ef5c2b61ddb401baf421a4acf0775a86f1
inyourtime <inyourtimeguy@gmail.com>
2026-05-01 20:48:03 +0700

    Remove Cloudflare adapter configuration from Astro setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -33,10 +32,4 @@ export default defineConfig({
           },
       },
 	],
-    adapter: cloudflare({
-      platformProxy: {
-          enabled: true
-      },
-      imageService: "cloudflare"
-  }),
 });
